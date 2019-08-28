53625699bbdd468e265ae1e6e6edb0cfff63e6ea
AJW <ajweber102@gmail.com>
2019-08-28 11:52:49 -0400

    built out controller index to take in articles

--- controllers/index.js
+++ controllers/index.js
@@ -3,3 +3,27 @@
 var express = require('express');
 var router = express.Router();
 var Article = require('../models/article.js');
+
+// Route
+router.get('/', function (req, res) {
+    Article
+        .find({})
+        .where('saved').equals(false)
+        .where('deleted').equals(false)
+        .sort('-date')
+        .limit(20)
+        .exec(function (error, articles) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                console.log(articles);
+                let hbsObj = {
+                    title: 'All the News',
+                    subtitle: 'Thats Fit to Scrape',
+                    articles: articles
+                };
+                res.render('index', hbsObj);
+            }
+        });
+});
