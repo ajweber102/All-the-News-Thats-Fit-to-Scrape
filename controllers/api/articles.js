092177ddb13decba19974b562a6d8cd538b6beb4
AJW <ajweber102@gmail.com>
2019-08-28 13:34:08 -0400

    Created functions for interacting with articles

--- controllers/api/articles.js
+++ controllers/api/articles.js
@@ -20,3 +20,80 @@ router.get('/', function (req, res) {
             }
         });
 });
+// Get for Saved Articles
+router.get('/saved', function (req, res) {
+    Article
+        .find({})
+        .where('saved').equals(true)
+        .where('deleted').equals(false)
+        .populate('notes')
+        .exec(function (error, docs) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.status(200).json(docs);
+            }
+        });
+});
+// Getting deleted articles
+router.get('/deleted', function (req, res) {
+    Article
+        .find({})
+        .where('deleted').equals(true)
+        .exec(function (error, docs) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.status(200).json(docs);
+            }
+        });
+});
+
+// Saving an article
+router.post('/save/:id', function (req, res) {
+    Article.findByIdAndUpdate(req.params.id, {
+        $set: { saved: true }
+    },
+        { new: true },
+        function (error, doc) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.redirect('/');
+            }
+        });
+});
+
+// Dismissing a scraped article
+router.delete('/dismiss/:id', function (req, res) {
+    Article.findByIdAndUpdate(req.params.id,
+        { $set: { deleted: true } },
+        { new: true },
+        function (error, doc) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.redirect('/');
+            }
+        });
+});
+
+// Delete funciton for saved articles
+router.delete('/:id', function (req, res) {
+    Article.findByIdAndUpdate(req.params.id,
+        { $set: { deleted: true } },
+        { new: true },
+        function (error, doc) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.redirect('/saved');
+            }
+        }
+    );
+});
