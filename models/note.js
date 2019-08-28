fcda4456274c60ae886f82ad7beca3328cdf2c39
AJW <ajweber102@gmail.com>
2019-08-28 11:17:54 -0400

    Added objects and means to export

--- models/note.js
+++ models/note.js
@@ -3,4 +3,16 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
-var NoteSchema = new Schema;
+var NoteSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+module.exports = Note;
