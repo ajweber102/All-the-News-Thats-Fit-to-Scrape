dc2ae673eb8ddfdaeb66b814063b61c83efe438e
AJW <ajweber102@gmail.com>
2019-08-28 12:04:48 -0400

    method for getting article sfrom database

--- controllers/api/articles.js
+++ controllers/api/articles.js
@@ -6,3 +6,17 @@ var express = require('express'),
     cheerio = require('cheerio'),
     Article = require('../../models/article'),
     Note = require('../../models/note');
+
+// Get database articles
+router.get('/', function (req, res) {
+    Article
+        .find({})
+        .exec(function (error, docs) {
+            if (error) {
+                console.log(error);
+                res.status(500);
+            } else {
+                res.status(200).json(docs);
+            }
+        });
+});
